e11a43a67d6511b8d4e10a873c036d953a8125ad
aaruni <rohanpandey749@gmail.com>
2023-10-06 07:56:30 +0530

    added hamburger menu and small fixes

--- src/components/Roadmap/Mapbox.jsx
+++ src/components/Roadmap/Mapbox.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import classes from "./styles.module.css";
 
-const Mapbox = () => {
+const Mapbox = ({roadmap}) => {
   return (
     <div className={classes.mapbox}>
       <div className={classes.mapbox_phase}>
-        <div className={classes.phase}>Phase 1</div>
+        <div className={classes.phase}>{roadmap.phase}</div>
         <div className={classes.phase_type}>Marketing</div>
       </div>
 
       <div className={classes.phase_desc}>
-        Marketing Website and whitepaper launch
+        {roadmap.heading}
       </div>
 
       <div className={classes.phase_para}>
-        In the first phase we will be launching the marketing website and our
-        whitepaper which will help users understand our vision.
+        {roadmap.description}
       </div>
     </div>
   );
--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -4,16 +4,51 @@ import roadmap from "../img/roadmap.svg";
 import { useEffect, useRef } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import dot from "../img/dot.png"
-import line1 from "../img/Line 3.png"
-import line2 from "../img/Line 6.png"
-import line3 from "../img/Line 7.png"
+import dot from "../img/dot.png";
+import line1 from "../img/Line 3.png";
+import line2 from "../img/Line 6.png";
+import line3 from "../img/Line 7.png";
 import Mapbox from "./Mapbox";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Roadmap = () => {
   useEffect(() => {
     AOS.init();
   }, []);
+
+  let roadmap = [
+    {
+      phase: "Phase 1",
+      heading: "Marketing Website and whitepaper launch",
+      description:
+        "In the first phase we will be launching the marketing website and our whitepaper which will help users understand our vision.",
+    },
+    {
+      phase: "Phase 2",
+      heading: "Mobile numbers launch 1",
+      description:
+        "First lot of numbers will be launched and will be claimable from the website.",
+    },
+    {
+      phase: "Phase 3",
+      heading: "Mobile numbers launch 2",
+      description:
+      "Second lot of numbers will be launched and will be claimable from the website."
+    },
+    {
+      phase: "Phase 4",
+      heading: "Calling, Messaging using telphone number launch",
+      description:
+        "At this phase we will launch the feature using which you can message/call others using their telphone number.",
+    },
+    {
+      phase: "Phase 5",
+      heading: "Other utilties launch",
+      description:
+        "Loging with telphone, video calling, OTP verification and all the other mobile number related utlities will be launched in this phase.",
+    },
+  ];
 
   return (
     <>
@@ -27,17 +62,14 @@ const Roadmap = () => {
             anonymously in TG community.
           </p>
         </div>
-        <img src={dot} className={classes.dot}/>
+        <div className={classes.map_cover}>
           <div className={classes.map_div}>
-            <Mapbox />
-            <Mapbox />
+            <Mapbox roadmap={roadmap[0]}/>
+            <Mapbox roadmap={roadmap[1]}/>
+            <Mapbox roadmap={roadmap[2]}/>
+            <Mapbox roadmap={roadmap[3]}/>
+            <Mapbox roadmap={roadmap[4]}/>
           </div>
-          <div className={classes.map_div}>
-            <Mapbox />
-            <Mapbox />
-        </div>
-        <div className={classes.map_div}>
-            <Mapbox />
         </div>
       </div>
     </>
